d996c3ec533b95267e0cc2d4bd163b0778c1db73
Knoester <ewout.knoester@wur.nl>
2021-02-19 11:40:39 +0300

    test Ewout's side

---
+++ depthgradient.R
@@ -0,0 +1,11 @@
+# ------------------------------------------
+# Analysis depth gradient (2019 - 2020): Bleaching and growth
+# Kristina Hrelja & Ewout Knoester
+# Created 05-Feb-2021
+# ------------------------------------------
+
+# Set R and packages
+rm(list=ls()) #Clear workspace
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #Set directory 
+
+# Test 19-02-2021
